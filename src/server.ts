dd9341660a423c761f3957f50025bfa862ca0aca
aayush-mhrzn7 <aayushmaharjan497@gmail.com>
2026-02-18 16:28:45 +0545

    init: bullmq service worker

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import WebSocketService from "./utils/ws.js";
+
 import "dotenv/config";
+import BullMQService from "./utils/bullmq.js";
 const app = express();
 const port = Number(process.env.PORT);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const wsService = new WebSocketService();
 app.use("/hls", express.static(path.join(__dirname, "./public/hls_output")));
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
--- src/utils/bullmq.ts
+++ src/utils/bullmq.ts
@@ -1,3 +1,70 @@
-class BullMQService {
-  constructor() {}
+import { Job, Queue, Worker } from "bullmq";
+
+export default class BullMQService {
+  private queue: Queue;
+  private worker: Worker;
+  readonly queue_name: string;
+
+  constructor(queue_name: string) {
+    console.log("BullMQ Service is active");
+    const connection = { host: "localhost", port: 6379 };
+
+    this.queue_name = queue_name;
+    this.queue = new Queue(queue_name, { connection });
+
+    this.worker = new Worker(
+      queue_name,
+      async (job: Job) => {
+        console.log("Received job:", job.data);
+        if (job.data.shouldFail) {
+          throw new Error("Failed");
+        }
+
+        for (let i = 0; i <= 100; i += 10) {
+          await new Promise((r) => setTimeout(r, 200));
+          job.updateProgress(i);
+        }
+      },
+      {
+        connection,
+        concurrency: 4,
+      },
+    );
+
+    this.worker.on("failed", (job, err) => {
+      if (job) {
+        console.log(
+          `Job ${job.id} failed attempt ${job.attemptsMade}/${job.opts.attempts}: ${err.message}`,
+        );
+      } else {
+        console.log("Failed job without job object:", err.message);
+      }
+    });
+
+    this.worker.on("completed", (job) => {
+      console.log(`Job ${job.id} completed`);
+    });
+  }
+
+  addToQueue(data: any) {
+    return this.queue.add(`${this.queue_name}-job`, data, {
+      removeOnComplete: true,
+      removeOnFail: true,
+      attempts: 2,
+      backoff: {
+        type: "fixed",
+        delay: 1000,
+      },
+    });
+  }
+
+  getJobProgress() {
+    this.worker.on("progress", (job, progress) => {
+      console.log(`Job ${job.id} progress: ${progress}%`);
+    });
+  }
 }
+
+const bullMqService = new BullMQService("video-processing");
+
+export { bullMqService };
